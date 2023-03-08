efcbe6652073a22f6d860dd13a87d5e85f2d5d41
DanielZajcev <126018275+DanielZajcev@users.noreply.github.com>
2023-03-08 19:45:00 +0100

    first commit

---
+++ test/src/App.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class App {
+    static JFrame frame = new JFrame("Tim stinkt");
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+
+    }
+}
