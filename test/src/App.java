99ec192cfe3341da4dd8beb089b3efc39dd96efc
DanielZajcev <126018275+DanielZajcev@users.noreply.github.com>
2023-03-08 21:38:09 +0100

    asdasd

--- test/src/App.java
+++ test/src/App.java
@@ -1,14 +1,24 @@
+import java.time.LocalDateTime;
+import java.io.*;
 import javax.swing.*;
 
 public class App {
     static JFrame frame = new JFrame("Tim stinkt");
+
+    static File file = new File("hahaha.txt");
+    static BufferedWriter writer = new BufferedWriter(new FileWriter(file));
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        System.out.println(LocalDateTime.now());
         frame.setSize(500, 500);
         frame.setVisible(true);
+        BufferedWriter writer = new BufferedWriter(new FileWriter("hahaha.txt"));        
+    }
 
+    public static LocalDateTime date(){
+        LocalDateTime time = new LocalDateTime();
+        return time;
     }
 }
