272ca3d21c1dd86059387670f0514a3d0adccef3
Daniel Zajcev <126018275+DanielZajcev@users.noreply.github.com>
2025-06-12 13:04:51 +0200

    Add .NET 9 console application

---
+++ DotNet9Console/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DotNet9Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET 9 console app!");
+        }
+    }
+}
